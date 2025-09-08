8255a9d6522e30d048b08774e3df4247dd55700e
Danilo Alves <danilo.alvess1902@gmail.com>
2025-09-08 14:46:11 -0300

    feat: add product gateway

---
+++ src/main/java/com/danilo_alves/marketplace_inventory/application/gateway/product/ProductGateway.java
@@ -0,0 +1,7 @@
+package com.danilo_alves.marketplace_inventory.application.gateway.product;
+
+import com.danilo_alves.marketplace_inventory.application.gateway.Gateway;
+import com.danilo_alves.marketplace_inventory.domain.entity.product.Product;
+
+public interface ProductGateway extends Gateway<Product> {
+}
