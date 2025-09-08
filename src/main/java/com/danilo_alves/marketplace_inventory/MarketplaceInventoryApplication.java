a45c2deb1e4cd1481a7d3bbca255d5d88ea5e59a
Danilo Alves <danilo.alvess1902@gmail.com>
2025-09-08 14:35:24 -0300

    Initial commit

---
+++ src/main/java/com/danilo_alves/marketplace_inventory/MarketplaceInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.danilo_alves.marketplace_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo_alves/marketplace_inventory/MarketplaceInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo_alves.marketplace_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
