7553f89aa89d3b4e00e83935a281c3a8caded013
Danilo Alves <danilo.alvess1902@gmail.com>
2025-09-13 19:57:29 -0300

    refactor: update ProductDomain with additional fields

--- src/main/java/com/danilo_alves/marketplace_inventory/domain/entity/product/ProductDomain.java
+++ src/main/java/com/danilo_alves/marketplace_inventory/domain/entity/product/ProductDomain.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+import java.util.Map;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -16,4 +19,13 @@ public class ProductDomain {
     private String description;
     private Double price;
     private Integer stock;
+
+    private String currencyId;
+    private String categoryId;
+    private String listingTypeId;
+    private String condition;
+    private String warranty;
+
+    private List<String> images;
+    private Map<String, String> attributes;
 }
