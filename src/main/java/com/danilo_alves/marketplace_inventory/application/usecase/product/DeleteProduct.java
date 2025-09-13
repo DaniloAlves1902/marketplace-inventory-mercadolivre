27b77ebec66e52065d3202d64989185181ca1327
Danilo Alves <danilo.alvess1902@gmail.com>
2025-09-12 22:30:43 -0300

    fix: fixing exception

--- src/main/java/com/danilo_alves/marketplace_inventory/application/usecase/product/DeleteProduct.java
+++ src/main/java/com/danilo_alves/marketplace_inventory/application/usecase/product/DeleteProduct.java
@@ -2,7 +2,6 @@ package com.danilo_alves.marketplace_inventory.application.usecase.product;
 
 import com.danilo_alves.marketplace_inventory.application.gateway.product.ProductGateway;
 import com.danilo_alves.marketplace_inventory.application.usecase.Usecase;
-import com.danilo_alves.marketplace_inventory.domain.exception.product.ProductNotFoundException;
 
 public class DeleteProduct implements Usecase<Long, Void> {
     private final ProductGateway productGateway;
@@ -14,7 +13,7 @@ public class DeleteProduct implements Usecase<Long, Void> {
     @Override
     public Void execute(Long id) {
         if (id == null) {
-            throw new ProductNotFoundException("id not found");
+            throw new IllegalArgumentException("id not found");
         }
 
         productGateway.delete(id);
