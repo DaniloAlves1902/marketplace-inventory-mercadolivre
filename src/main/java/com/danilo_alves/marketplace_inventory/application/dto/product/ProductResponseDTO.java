05a9fbe06b655f22c182f22de98805b84edcfb0f
Danilo Alves <danilo.alvess1902@gmail.com>
2025-09-19 09:30:29 -0300

    feat: add product response dto

---
+++ src/main/java/com/danilo_alves/marketplace_inventory/application/dto/product/ProductResponseDTO.java
@@ -0,0 +1,21 @@
+package com.danilo_alves.marketplace_inventory.application.dto.product;
+
+import java.util.List;
+import java.util.Map;
+
+public record ProductResponseDTO(
+        Long id,
+        String sku,
+        String name,
+        String description,
+        Double price,
+        Integer stock,
+        String currencyId,
+        String categoryId,
+        String listingTypeId,
+        String condition,
+        String warranty,
+        List<String> images,
+        Map<String, String> attributes
+) {
+}
