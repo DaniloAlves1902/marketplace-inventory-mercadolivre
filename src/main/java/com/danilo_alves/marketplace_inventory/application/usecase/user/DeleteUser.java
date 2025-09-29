ae94f4dc616b603b2f2716fc6bd193140d155a56
Danilo Alves <danilo.alvess1902@gmail.com>
2025-09-29 16:49:40 -0300

    feat: add delete user use case and its respective interface

---
+++ src/main/java/com/danilo_alves/marketplace_inventory/application/usecase/user/DeleteUser.java
@@ -0,0 +1,6 @@
+package com.danilo_alves.marketplace_inventory.application.usecase.user;
+
+import com.danilo_alves.marketplace_inventory.application.usecase.UseCase;
+
+public interface DeleteUser extends UseCase<String, Void> {
+}
