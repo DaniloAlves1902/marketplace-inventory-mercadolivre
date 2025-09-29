68178a47db373e16043d753efba74f0b00083fca
Danilo Alves <danilo.alvess1902@gmail.com>
2025-09-29 16:50:40 -0300

    feat: add update user use case and its respective interface

---
+++ src/main/java/com/danilo_alves/marketplace_inventory/application/usecase/user/UpdateUser.java
@@ -0,0 +1,7 @@
+package com.danilo_alves.marketplace_inventory.application.usecase.user;
+
+import com.danilo_alves.marketplace_inventory.application.usecase.UseCase;
+import com.danilo_alves.marketplace_inventory.domain.entity.user.UserDomain;
+
+public interface UpdateUser extends UseCase<UserDomain, UserDomain> {
+}
