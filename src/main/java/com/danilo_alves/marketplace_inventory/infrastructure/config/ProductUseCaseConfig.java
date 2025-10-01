a37d51c0fbddd4c3604db0728b215f469985009e
Danilo Alves <danilo.alvess1902@gmail.com>
2025-10-01 08:44:49 -0300

    refactor: renamed product bean configuration for better code organization

--- src/main/java/com/danilo_alves/marketplace_inventory/infrastructure/config/ProductUseCaseConfig.java
+++ src/main/java/com/danilo_alves/marketplace_inventory/infrastructure/config/ProductUseCaseConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeanConfiguration {
+public class ProductUseCaseConfig {
 
     @Bean
     public CreateProduct createProduct(ProductGateway productGateway) {
